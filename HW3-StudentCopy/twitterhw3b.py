ec4c5592e87ad9e600e20b2435c6b677a3d1f7f3
geolu@umich.edu <geolu@umich.edu>
2016-11-08 16:20:28 -0500

    finished twitterhw3b part

--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -6,7 +6,44 @@
 # 3) Print the average polarity of the results
 
 # Be prepared to change the search term during demo.
+import tweepy
+from textblob import TextBlob
+import twittertoken
+
+# Unique code from Twitter
+access_token = twittertoken.access_token
+access_token_secret = twittertoken.access_token_secret
+consumer_key = twittertoken.consumer_key
+consumer_secret = twittertoken.consumer_secret
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+public_tweets = api.search('Golden State Warriors')
 
 
-print("Average subjectivity is")
-print("Average polarity is")
+avg_subjectivity = 0.0
+avg_polarity = 0.0
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	# print(analysis.sentiment)
+	# print(analysis.sentiment.polarity)
+	avg_subjectivity += analysis.sentiment.subjectivity
+	avg_polarity += analysis.sentiment.polarity
+
+
+#polarity -- measures how positive or negative
+#subjectivity -- measures how factual.
+
+#1 Sentiment Analysis - Understand and Extracting Feelings from Data
+
+
+print()
+print("Average subjectivity is", avg_subjectivity)
+print("Average polarity is", avg_polarity)
