436744ab4239f80e014674352e3aaae00dd0b1ae
geolu@umich.edu <geolu@umich.edu>
2016-11-08 09:19:14 -0500

    finished twitterhw3a and made a separate file to keep authentication tokens/secrets secure

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -4,5 +4,29 @@
 
 # You will demo this live for grading.
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+# print("""No output necessary although you 
+# 	can print out a success/failure message if you want to.""")
+
+import tweepy
+from textblob import TextBlob
+import os
+import twittertoken
+
+# Unique code from Twitter
+access_token = twittertoken.access_token
+access_token_secret = twittertoken.access_token_secret
+consumer_key = twittertoken.consumer_key
+consumer_secret = twittertoken.consumer_secret
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+img = os.path.abspath('kevin.jpg')
+try:
+	api.update_with_media(img, status="Hi David #UMSI-206 #Proj3")
+	print("Success!")
+except:
+	print("Failed! :(")
