a371aaf5d32d20262a5c2cf8d33883ed3e29a57e
geolu@umich.edu <geolu@umich.edu>
2016-11-09 12:55:39 -0500

    used prettify after going to office hours, not done yet

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -17,13 +17,54 @@ from bs4 import BeautifulSoup
 
 base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
 r = requests.get(base_url)
-soup = BeautifulSoup(r.text, "lxml")
+soup = BeautifulSoup(r.text, 'html.parser')
+soup_str = str(soup)
 
-picture = soup.find_all('img')
+
+html_text = soup.prettify() # turns it into a string too
+# print(text)
+
+
+
+
+for line in text:
+	print("hi")
+
+
+amazing_change = 
+
+
+img = os.path.abspath('')
+html_text = re.sub('logo2.png', img, html_text)
+
+# could either use RegEx for this
+# or the way Paco did it
+
+# print(soup)
+
+img_html = soup.find_all('img')
+# print(img_html)
+
+img = soup('img')
+# print(img)
 # picture = soup.find_all('div', {'class': 'field-item even'})
 
-print(picture)
+# print(img_html)
+
+# run the replaceWith function?
 
 
 
 fout = open("index.html", 'w')
+
+
+fout.write(text)
+
+# fout.write(soup_str) # this just writes the whole html in
+# # write into the html file
+
+# # you can use regex or not
+
+
+
+fout.close()
