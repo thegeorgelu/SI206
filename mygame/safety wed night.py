7e2b6977fdc0d5b0213d4044c56f1e40846ce190
geolu@umich.edu <geolu@umich.edu>
2016-11-30 19:11:03 -0500

    safety wed night file has the code from game.py that is updated to the point of registering for classes

--- mygame/safety wed night.py
+++ mygame/safety wed night.py
@@ -3,10 +3,9 @@ from pygame import *
 from pygame.sprite import *
 from random import *
 import math
-from pygame.locals import Rect, DOUBLEBUF, QUIT, K_ESCAPE, KEYDOWN, K_DOWN, \
-K_LEFT, K_UP, K_RIGHT, KEYUP, K_LCTRL, K_RETURN, FULLSCREEN, K_SPACE
 
 pygame.init();
+easy_mode_flag = True
 
 # RGB colors
 white = (255,255,255)
@@ -17,16 +16,21 @@ blue = (0, 0, 255)
 violet = (238, 130, 238)
 medium_violet = (219, 112, 147)
 violet_red = (208, 32, 144)
+aqua = (127, 255, 212)
 
 #position update vars
 x_delta = 0
 y_delta = 0
+
+# pygame clock
 clock = pygame.time.Clock()
 
+credits_timer = 5000
+
 display_width = 800
-display_height = 800
+display_height = 720
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption("Survival Game")
+pygame.display.set_caption("The Survival Game")
 
 class Player(Sprite):
 	def __init__(self):
@@ -36,7 +40,7 @@ class Player(Sprite):
 		self.width = 20
 		self.rect = pygame.Rect(self.x_pos, self.y_pos, self.length, self.width)
 
-		self.lives = 10
+		self.lives = 15
 		self.points = 0
 
 	def move(self, x, y):
@@ -74,14 +78,17 @@ class Prize(Sprite):
 		self.rect = pygame.Rect(self.x_pos, self.y_pos, self.length, self.width)
 
 	def move(self):
-		randX = (randint(20, display_width - 20) // 20) * 20
-		randY = (randint(20, display_width - 20) // 20) * 20
-		self.rect.center = (randX ,randY)
+		randX = (randint(20, display_width - 20) // 20) * 20 # this should be rounding to multiples of 20 correctly
+		randY = (randint(20, display_height - 20) // 20) * 20
+		while randX >= 120 and randX <= 650 and randY <= 20:
+			randX = (randint(20, display_width - 20) // 20) * 20
+			randY= (randint(20, display_height - 20) // 20) * 20
+		self.rect.center = (randX, randY)
 
 class Enemy(Sprite):
 	def __init__(self):
 		Sprite.__init__(self)
-		# self.image = image.load("bat.bmp").convert_alpha()
+		# self.image = image.load("enemy.bmp").convert_alpha()
 		self.image = image.load("poop.bmp").convert_alpha()
 		self.rect = self.image.get_rect()
 		self.rect.center = (400, 200)
@@ -106,13 +113,22 @@ enemies = RenderPlain(enemy)
 
 f = font.Font(None, 30)
 
+# play music in the background
 pygame.mixer.music.load("soundtrack.wav")
 pygame.mixer.music.set_volume(0.6)
 pygame.mixer.music.play(-1)
 
-gameExit = False
+# enemy delay values
 time_check = 3000
 enemy_time_check = 5000
+
+update_life_1 = True
+update_life_2 = True
+update_life_3 = True
+update_life_4 = True
+update_life_5 = True
+
+gameExit = False
 while not gameExit:
 	gameDisplay.fill(white)
 
@@ -132,7 +148,8 @@ while not gameExit:
 			if event.key == pygame.K_DOWN:
 				y_delta += 10
 
-		if player.check_collision(player, enemy): # lags when hitting this guy now, not sure if i should keep it in the for loop or take it out to underneath
+		# working fine for now (with the poop.bmp)
+		if player.check_collision(player, enemy):
 			print("player hit the bat!")
 			mixer.Sound("enemyhit.wav").play()
 			player.lives -= 3
@@ -149,36 +166,56 @@ while not gameExit:
 	elif player.rect.y > display_height:
 		player.rect.y = 0
 
+	# check if player collides with a bad block
 	for bad_block in bad_block_list:
 		if player.check_collision(player, bad_block):
 			print("collision!")
-			mixer.Sound("badblock.wav").play()
+			mixer.Sound("badblockhit.wav").play()
 			bad_block.move()
+			prize.move() # TROLL
 			player.lives -= 1
 
+	# check if player collides with the prize
 	if player.check_collision(player, prize):
 		print("player hit the prize!")
 		mixer.Sound("coin.wav").play()
 		player.points += 1
 		prize.move()
 
+	# adds a new block faster and faster
 	if len(bad_block_list) < 1000 and pygame.time.get_ticks() > time_check: # this way adds a new block every few seconds
 		temp_bad_block = BadBlock()
 		temp_bad_block.rect.x = (randint(20, display_width - 20) // 20) * 20 # this should be rounding to multiples of 20 correctly
 		temp_bad_block.rect.y = (randint(20, display_height - 20) // 20) * 20
-		while temp_bad_block.rect.x >= 200 and temp_bad_block.rect.x <= 550 and temp_bad_block.rect.y <= 20:
+		while temp_bad_block.rect.x >= 120 and temp_bad_block.rect.x <= 650 and temp_bad_block.rect.y <= 20:
 			temp_bad_block.rect.x = (randint(20, display_width - 20) // 20) * 20
 			temp_bad_block.rect.y = (randint(20, display_height - 20) // 20) * 20
 		bad_block_list.append(temp_bad_block)
-		if pygame.time.get_ticks() < 10000:
-			time_check += 800
-		elif pygame.time.get_ticks() < 16000:
-			time_check += 600
-		elif pygame.time.get_ticks() < 22000:
-			time_check += 400
-		else:
-			time_check += 200
 
+		# easy mode:
+		if easy_mode_flag:
+			if pygame.time.get_ticks() < 10000:
+				time_check += 4000
+			elif pygame.time.get_ticks() < 30000:
+				time_check += 2000
+			elif pygame.time.get_ticks() < 60000:
+				time_check += 1000
+			else:
+				time_check += 600
+		# hard mode
+		else:
+			if pygame.time.get_ticks() < 10000:
+				time_check += 800
+			elif pygame.time.get_ticks() < 16000:
+				time_check += 600
+			elif pygame.time.get_ticks() < 22000:
+				time_check += 400
+			else:
+				time_check += 200
+
+
+
+	# moves the enemy faster and faster
 	if pygame.time.get_ticks() > enemy_time_check:
 		enemy.move()
 		if pygame.time.get_ticks() < 10000:
@@ -190,19 +227,53 @@ while not gameExit:
 		else:
 			enemy_time_check += 2000
 
+	# give lives back to players as they collect more points
+	if player.points == 5:
+		if update_life_1:
+			player.lives += 1
+			update_life_1 = False
+	elif player.points == 10:
+		if update_life_2:
+			player.lives += 2
+			update_life_2 = False
+	elif player.points == 15:
+		if update_life_3:
+			player.lives += 3
+			update_life_3 = False
+	elif player.points == 20:
+		if update_life_4:
+			player.lives += 5
+			update_life_4 = False
+	elif player.points == 25:
+		if update_life_5:
+			player.lives += 10
+			update_life_5 = False
+
+	# run game over stuff
 	if player.lives <= 0:
 		print("game over!!!")
 		print(player.points)
 		gameExit = True
 
+		# need to somehow get rid of the rest of the screen
+
+		myfont = pygame.font.SysFont("monospace", 50)
+		credits = myfont.render("GAME OVER!", 1, black)
+		gameDisplay.blit(credits, (370, 360))
+
+		if credits_timer:
+			credits_timer -= 1
+		else:
+			gameExit = True
+
 	# show number of seconds elapsed, number of lives, and number of points
 	if player.lives > 0:
 		time_text = f.render("Time Elapsed: " + str(pygame.time.get_ticks() / 1000), False, black)
-		gameDisplay.blit(time_text, (100, 0))
+		gameDisplay.blit(time_text, (120, 0))
 		lives_text = f.render("Current Lives: " + str(player.lives), False, black)
 		gameDisplay.blit(lives_text, (350, 0))
 		points_text = f.render("Points: " + str(player.points), False, black)
-		gameDisplay.blit(points_text, (600, 0))
+		gameDisplay.blit(points_text, (550, 0))
 
 	enemies.update()
 	enemies.draw(gameDisplay)
@@ -210,6 +281,7 @@ while not gameExit:
 	pygame.draw.rect(gameDisplay, green, prize.rect)
 
 	# color of bad_blocks gets more and more red as time goes on
+	# and then things start getting crazy
 	if pygame.time.get_ticks() < 15000:
 		for bad_block in bad_block_list:
 			pygame.draw.rect(gameDisplay, violet, bad_block.rect)
@@ -219,12 +291,38 @@ while not gameExit:
 	elif pygame.time.get_ticks() < 45000:
 		for bad_block in bad_block_list:
 			pygame.draw.rect(gameDisplay, violet_red, bad_block.rect)
-	else:
+	elif pygame.time.get_ticks() < 60000:
 		for bad_block in bad_block_list:
 			pygame.draw.rect(gameDisplay, red, bad_block.rect)
+	# things start to get crazy
+	# flashing lights mode haha
+	elif pygame.time.get_ticks() < 90000:
+		if pygame.time.get_ticks() % 4 == 0:
+			for bad_block in bad_block_list:
+				pygame.draw.rect(gameDisplay, violet, bad_block.rect)
+		else:
+			for bad_block in bad_block_list:
+				pygame.draw.rect(gameDisplay, aqua, bad_block.rect)
+	else:
+		if pygame.time.get_ticks() % 4 == 0:
+			for bad_block in bad_block_list:
+				pygame.draw.rect(gameDisplay, violet, bad_block.rect)
+		else:
+			for bad_block in bad_block_list:
+				pygame.draw.rect(gameDisplay, green, bad_block.rect)
+
 
 	pygame.display.update()
 	clock.tick(30)
+
+
+# once game is over
+gameDisplay.fill(white)
+myfont = pygame.font.SysFont("monospace", 50)
+credits = myfont.render("GAME OVER!", 1, black)
+gameDisplay.blit(credits, (370, 360))
+while credits_timer:
+	credits_timer -= 1
 
 #required
 pygame.quit()
